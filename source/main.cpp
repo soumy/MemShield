e7d3a81c80cc7d82dbc2f10fba1e30963e55acea
Soumyadipta Das <soumyadipta_das@yahoo.com>
2017-09-22 10:01:51 +0530

    First working version
    This is the first working version.

--- source/main.cpp
+++ source/main.cpp
@@ -6,8 +6,6 @@
 #include <winternl.h>
 #include <intrin.h>
 
-//#define _BUILD_FOR_WINXP_X86
-
 void SetHooks();
 
 BOOL WINAPI DllMain(_In_  HINSTANCE hinstDLL,_In_  DWORD fdwReason,_In_  LPVOID lpvReserved)
@@ -46,100 +44,99 @@ typedef enum _MEMORY_INFORMATION_CLASS {
   MemoryBasicInformation
 } MEMORY_INFORMATION_CLASS;
 
-static __declspec(naked) NTSTATUS __stdcall NtVirtualQuery(HANDLE ProcessHandle, PVOID BaseAddress, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformation, SIZE_T MemoryInformationLength, PSIZE_T ReturnLength)
-{
-	__asm{
-		call next
-		ret 0x18
-next:
-#ifdef _BUILD_FOR_WINXP_X86
-		mov eax, 0x0B2
-#else
-		mov eax, 0x10B
-#endif
-		mov edx, esp
-	   _emit 0x0F
-	   _emit 0x34
-	   ret 0x18
-	};
+typedef NTSTATUS (__stdcall *MyNtVirtualQuery)(HANDLE ProcessHandle, PVOID BaseAddress, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformation, SIZE_T MemoryInformationLength, PSIZE_T ReturnLength);
+static MyNtVirtualQuery NtVirtualQuery = NULL;
 
-}
+typedef NTSTATUS (__stdcall *MyNtVirtualProtect_Type)(HANDLE hProcess, LPVOID lpAddress, SIZE_T* pdwSize, DWORD flNewProtect, PDWORD lpflOldProtect);
+MyNtVirtualProtect_Type MyNtVirtualProtect = NULL;
 
+#define NOP   __asm{nop}
+#define NOPS NOP NOP NOP NOP NOP NOP NOP NOP NOP NOP
+#define NOPSLIDE NOPS NOPS NOPS NOPS NOPS
 static __declspec(naked) NTSTATUS __stdcall NtVirtualProtect(HANDLE hProcess, LPVOID lpAddress, SIZE_T* pdwSize, DWORD flNewProtect, PDWORD lpflOldProtect)
 {
-	__asm{
-		call next
-		ret 0x14
-next:
-#ifdef _BUILD_FOR_WINXP_X86
-		mov eax, 0x89
-#else
-		mov eax, 0x0d7
-#endif
-		mov edx, esp
-	   _emit 0x0F
-	   _emit 0x34
-	   ret 0x14
-	};
+	NOPSLIDE;
+	__asm{ret};
+}
+
+static __declspec(naked) NTSTATUS __stdcall NtAllocateVirtualMemory(HANDLE ProcessHandle,PVOID *BaseAddress, ULONG_PTR ZeroBits, PSIZE_T RegionSize, ULONG AllocationType, ULONG Protect)
+{
+	NOPSLIDE;
+	__asm{ret};
 }
 
 static __declspec(naked) NTSTATUS __stdcall hook_NtAllocateVirtualMemory(HANDLE ProcessHandle,PVOID *BaseAddress, ULONG_PTR ZeroBits, PSIZE_T RegionSize, ULONG AllocationType, ULONG Protect)
 {
+
+#ifdef _WIN32
 	__asm {  
       push ebp  
       mov ebp, esp
 	};
-
-	if(ProcessHandle == (HANDLE)-1)
+#endif
+	
+	if(Protect & PAGE_EXECUTE)
 	{
-		if(Protect & PAGE_EXECUTE)
-		{
-			Protect &= ~PAGE_EXECUTE;
-			Protect |= PAGE_READONLY;
-		}
-		if(Protect & PAGE_EXECUTE_READ)
-		{
-			Protect &= ~PAGE_EXECUTE_READ;
-			Protect |= PAGE_READONLY; 
-		}
-		if(Protect & PAGE_EXECUTE_READWRITE)
-		{
-			Protect &= ~PAGE_EXECUTE_READWRITE;
-			Protect |= PAGE_READWRITE;
-		}
-		if(Protect & PAGE_EXECUTE_WRITECOPY)
-		{
-			Protect &= ~PAGE_EXECUTE_WRITECOPY;
-			Protect |= PAGE_WRITECOPY;
-		}
+		Protect &= ~PAGE_EXECUTE;
+		Protect |= PAGE_READONLY;
 	}
-	SKIP:
+	if(Protect & PAGE_EXECUTE_READ)
+	{
+		Protect &= ~PAGE_EXECUTE_READ;
+		Protect |= PAGE_READONLY; 
+	}
+	if(Protect & PAGE_EXECUTE_READWRITE)
+	{
+		Protect &= ~PAGE_EXECUTE_READWRITE;
+		Protect |= PAGE_READWRITE;
+	}
+	if(Protect & PAGE_EXECUTE_WRITECOPY)
+	{
+		Protect &= ~PAGE_EXECUTE_WRITECOPY;
+		Protect |= PAGE_WRITECOPY;
+	}
+	
+#ifdef _WIN32
 	__asm{
 		mov esp, ebp
 		pop ebp 
-		call next 
-		ret 0x18
-next:
-#ifdef _BUILD_FOR_WINXP_X86
-		mov eax, 0x11
-#else
-		mov eax, 0x13
-#endif
-		mov edx, esp
-	   _emit 0x0F
-	   _emit 0x34
-	   ret 0x18
+		jmp NtAllocateVirtualMemory
+
 	};
+#endif
 }
 
+static int stack_space = sizeof(MEMORY_BASIC_INFORMATION) + sizeof(PVOID);
 static __declspec(naked) NTSTATUS __stdcall hook_NtVirtualProtect(HANDLE hProcess, LPVOID* lpAddress, SIZE_T* pdwSize, DWORD flNewProtect, PDWORD lpflOldProtect)
 {
-	__asm {  
+#ifdef _WIN32
+	__asm {
+	  pushad
       push ebp  
       mov ebp, esp
+	  sub esp, stack_space
 	};
-
-	if(hProcess == (HANDLE)-1)
+#endif
+	MEMORY_BASIC_INFORMATION gmbi;
+	memset(&gmbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,*lpAddress,MemoryBasicInformation,&gmbi,sizeof(MEMORY_BASIC_INFORMATION),NULL);
+	
+	if((gmbi.Type&MEM_IMAGE))
+	{
+		
+		if(flNewProtect & PAGE_EXECUTE_READWRITE)
+		{
+			flNewProtect &= ~PAGE_EXECUTE_READWRITE;
+			flNewProtect |= PAGE_READWRITE;
+		}
+		if(flNewProtect & PAGE_EXECUTE_WRITECOPY)
+		{
+			flNewProtect &= ~PAGE_EXECUTE_WRITECOPY;
+			flNewProtect |= PAGE_WRITECOPY;
+		}
+		
+	}
+	else
 	{
 		if(flNewProtect & PAGE_EXECUTE)
 		{
@@ -162,23 +159,17 @@ static __declspec(naked) NTSTATUS __stdcall hook_NtVirtualProtect(HANDLE hProces
 			flNewProtect |= PAGE_WRITECOPY;
 		}
 	}
-	SKIP:
+
+#ifdef _WIN32
 	__asm{
+		add esp, stack_space
 		mov esp, ebp
 		pop ebp
-		call next
-		ret 0x14
-next:
-#ifdef _BUILD_FOR_WINXP_X86
-		mov eax, 0x89
-#else
-		mov eax, 0x0d7
-#endif
-		mov edx, esp
-	   _emit 0x0F
-	   _emit 0x34
-	   ret 0x14
+		popad
+		jmp NtVirtualProtect
 	};
+#endif
+
 }
 
 static NTSTATUS __stdcall DoImg(NTSTATUS status, HANDLE ProcessHandle, PVOID *Pbase)
@@ -245,6 +236,24 @@ ViewShare = 1,
 ViewUnmap = 2
 } SECTION_INHERIT;
 
+static __declspec(naked) NTSTATUS __stdcall NtMapViewofSection(
+	HANDLE          SectionHandle,
+	HANDLE          ProcessHandle,
+	PVOID           *BaseAddress,
+	ULONG_PTR       ZeroBits,
+	SIZE_T          CommitSize,
+	PLARGE_INTEGER  SectionOffset,
+	PSIZE_T         ViewSize,
+	SECTION_INHERIT InheritDisposition,
+	ULONG           AllocationType,
+	ULONG           Win32Protect
+		  )
+{
+
+	NOPSLIDE;
+	__asm{ret};
+}
+
 static __declspec(naked) NTSTATUS __stdcall hook_NtMapViewofSection(
 	HANDLE          SectionHandle,
 	HANDLE          ProcessHandle,
@@ -258,24 +267,16 @@ static __declspec(naked) NTSTATUS __stdcall hook_NtMapViewofSection(
 	ULONG           Win32Protect
 		  )
 {
+#ifdef _WIN32
 	__asm{
-		call next
+		jmp NtMapViewofSection
 		push [esp+0xc] //*base
 		push [esp+0xc] //processhandle
 		push eax       //status
 		call DoImg
 		ret 0x28
-next:
-#ifdef _BUILD_FOR_WINXP_X86
-		mov eax, 0x6C
-#else
-		mov eax, 0x0A8
-#endif
-		mov edx, esp
-	   _emit 0x0F
-	   _emit 0x34
-	   ret 0x28
 	};
+#endif
 }
 
 void SetHooks()
@@ -283,34 +284,52 @@ void SetHooks()
 	DWORD Old_Protect=NULL,temp=NULL;
 	MEMORY_BASIC_INFORMATION mbi = {0};
 
-	DWORD *lpNtProtectVirtMem = (DWORD*) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtProtectVirtualMemory");
-	if(lpNtProtectVirtMem)
-	{
-		NtVirtualQuery((HANDLE)-1,lpNtProtectVirtMem,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
-		*(DWORD*)((DWORD)lpNtProtectVirtMem+1) = ( (DWORD)&hook_NtVirtualProtect - ((DWORD)lpNtProtectVirtMem+5) );
-		*((char*)lpNtProtectVirtMem) = (char)0xe9;
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
-	}
+	NtVirtualQuery = (MyNtVirtualQuery) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtQueryVirtualMemory");
+	PVOID lpNtProtectVirtMem = (PVOID) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtProtectVirtualMemory");
+	PVOID lpNtAllocateVirtMem = (PVOID) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtAllocateVirtualMemory");
+	PVOID lpNtmapViewOfSection = (PVOID) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtMapViewOfSection");
+	
 
-	DWORD *lpNtAllocateVirtMem = (DWORD*) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtAllocateVirtualMemory");
-	if(lpNtAllocateVirtMem)
-	{
-		NtVirtualQuery((HANDLE)-1,lpNtAllocateVirtMem,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
-		*(DWORD*)((DWORD)lpNtAllocateVirtMem+1) = ( (DWORD)&hook_NtAllocateVirtualMemory - ((DWORD)lpNtAllocateVirtMem+5) );
-		*((char*)lpNtAllocateVirtMem) = (char)0xe9;
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
-	}
+	MyNtVirtualProtect = (MyNtVirtualProtect_Type) lpNtProtectVirtMem;
+	if(!NtVirtualQuery || !MyNtVirtualProtect || !lpNtAllocateVirtMem || !lpNtmapViewOfSection) return;
+
+	NtVirtualQuery((HANDLE)-1,&NtVirtualProtect,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	memcpy(&NtVirtualProtect,lpNtProtectVirtMem,50);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
+
+	memset(&mbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,&NtAllocateVirtualMemory,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	memcpy(&NtAllocateVirtualMemory,lpNtAllocateVirtMem,50);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
+
+	memset(&mbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,&NtMapViewofSection,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	memcpy(&NtMapViewofSection,lpNtmapViewOfSection,50);
+	MyNtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
 	
-	DWORD *lpNtmapViewOfSection = (DWORD*) GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")),"NtMapViewOfSection");
-	if(lpNtmapViewOfSection)
-	{
-		NtVirtualQuery((HANDLE)-1,lpNtmapViewOfSection,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
-		*(DWORD*)((DWORD)lpNtmapViewOfSection+1) = ( (DWORD)&hook_NtMapViewofSection - ((DWORD)lpNtmapViewOfSection+5) );
-		*((char*)lpNtmapViewOfSection) = (char)0xe9;
-		NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
-	}
+	memset(&mbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,lpNtAllocateVirtMem,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	*(UINT_PTR*)((UINT_PTR)lpNtAllocateVirtMem+1) = ( (UINT_PTR)&hook_NtAllocateVirtualMemory - ((UINT_PTR)lpNtAllocateVirtMem+5) );
+	*((char*)lpNtAllocateVirtMem) = (char)0xe9;
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
 	
+	memset(&mbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,lpNtProtectVirtMem,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	*(UINT_PTR*)((DWORD)lpNtProtectVirtMem+1) = ( (UINT_PTR)&hook_NtVirtualProtect - ((UINT_PTR)lpNtProtectVirtMem+5) );
+	*((char*)lpNtProtectVirtMem) = (char)0xe9;
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
+	
+	memset(&mbi,0,sizeof(MEMORY_BASIC_INFORMATION));
+	NtVirtualQuery((HANDLE)-1,lpNtmapViewOfSection,MemoryBasicInformation,&mbi,sizeof(mbi),NULL);
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),PAGE_EXECUTE_READWRITE,&Old_Protect);
+	*(DWORD*)((DWORD)lpNtmapViewOfSection+1) = ( (DWORD)&hook_NtMapViewofSection - ((DWORD)lpNtmapViewOfSection+5) );
+	*((char*)lpNtmapViewOfSection) = (char)0xe9;
+	NtVirtualProtect((HANDLE)-1,&(mbi.BaseAddress),&(mbi.RegionSize),Old_Protect,&temp);
+	
+	FlushInstructionCache((HANDLE)-1,NULL,0);
 }
